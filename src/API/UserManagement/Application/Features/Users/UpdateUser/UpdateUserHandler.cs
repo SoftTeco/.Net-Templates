123d456dd14ced59a29f9883f8676cfd5f4e09ee
Nastya Abakanovich <nastya.abakanovich@gmail.com>
2024-10-10 11:22:04 +0300

    feat: added user updating

---
+++ src/API/UserManagement/Application/Features/Users/UpdateUser/UpdateUserHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OneOf.Types;
+using OneOf;
+using UserManagement.Application.Data;
+
+namespace Application.Features.Users.UpdateUser;
+
+public class UpdateUserHandler
+{
+    private readonly IUsersDbContext _usersDbContext;
+
+    public UpdateUserHandler(IUsersDbContext usersDbContext)
+    {
+        _usersDbContext = usersDbContext;
+    }
+
+    public async Task<OneOf<Success, NotFound>> Handle(Guid userId, UpdateUserRequest request, CancellationToken cancellationToken)
+    {
+        var user = await _usersDbContext.Users
+            .SingleOrDefaultAsync(u => u.Id == userId && !u.IsDeleted, cancellationToken);
+        if (user == null)
+        {
+            return new NotFound();
+        }
+
+        user.FirstName = request.FirstName;
+        user.LastName = request.LastName;
+        user.Email = request.Email; 
+        await _usersDbContext.SaveChangesAsync(cancellationToken);
+        return new Success();
+    }
+}
---
+++ src/API/UserManagement/Web/Features/Users/UpdateUser/UpdateUserController.cs
@@ -0,0 +1,27 @@
+﻿using Application.Features.Users.UpdateUser;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Features.Users.UpdateUser;
+
+[ApiController]
+[Route("api/users/{userId:guid}")]
+public class UpdateUserController : ControllerBase
+{
+    private readonly UpdateUserHandler _handler;
+
+    public UpdateUserController(UpdateUserHandler handler)
+    {
+        _handler = handler;
+    }
+
+    [HttpPatch]
+    public async Task<IResult> UpdateUser([FromRoute] Guid userId, 
+        [FromBody] UpdateUserRequest request, CancellationToken cancellationToken)
+    {
+        var result = await _handler.Handle(userId, request, cancellationToken);
+        return result.Match<IResult>(
+            _ => TypedResults.Ok(),
+            _ => TypedResults.NotFound());
+    }
+}
